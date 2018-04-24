ed78ee139ba4d5e48b28001c2a08e51589085470
LeeSangHoon <dl57934@naver.com>
2018-04-24 15:11:20 +0900

    fix news list number

--- get Sports Server/getHtml.js
+++ get Sports Server/getHtml.js
@@ -14,17 +14,17 @@ function getHtmlData(url,socket){
             var news = newsList.split('\n');
             var newsUrl1 = new Array();
             var newsUrl2 = new Array();
-
+            var liLength = $('.home_news_list > li').length;
             for (var i = 1;i<=10;i++)
                 newsUrl1.push( document.querySelector("#content > div > div.home_grid > div.content > div.home_article > div.home_news > ul:nth-child(2) > li:nth-child("+i+") > a").href);
-            for (var i =1 ;i<=10;i++)
-                newsUrl2.push(document.querySelector("#content > div > div.home_grid > div.content > div.home_article > div.home_news > ul.home_news_list.division > li:nth-child(" + i + ") > a").href);
+
+            for (var i =1 ;i <= liLength-10;i++)
+                 newsUrl2.push(document.querySelector("#content > div > div.home_grid > div.content > div.home_article > div.home_news > ul.home_news_list.division > li:nth-child(" + i + ") > a").href);
             var newsUrl = newsUrl1.concat(newsUrl2);
-            var newsInfo = {'news':news,'newsUrl':newsUrl}
+            var newsInfo = {'news':news,'newsUrl':newsUrl,'newsLength':liLength}
             return newsInfo;
         });
     }).then(function (content) {
-
         socket.emit('newsInfo',content);
     });
 }
--- getSports/newsInfo.js
+++ getSports/newsInfo.js
@@ -1,13 +1,10 @@
-var queryInfo = null;
 socket.on('newsInfo',function (data) {
-    var name= []
-    for(var i = 0 ;i<20;i++) {
+    var name= [];
+    for(var i = 0 ;i<data['newsLength'];i++)
         name.push(data['news'][i+2]+'\n');
-    }
     var ol = document.getElementById('ol');
-    for (var i = 0; i < 20; i++){
+    for (var i = 0; i < data['newsLength']; i++){
         var li = document.createElement('li');
-
         ol.appendChild(li);
         var a = document.createElement('a');
         var text = document.createTextNode(name[i]);
@@ -19,7 +16,6 @@ socket.on('newsInfo',function (data) {
    $("li").click(function () {
        var a = $("li").index(this);
        var href = $("li:eq(" + a + ") > a").attr("href");
-       alert(a);
         chrome.tabs.create({url:href});
    });
 });
