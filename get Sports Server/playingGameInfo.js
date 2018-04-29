57e3ac3ffd38e92e572174db5abdd5ce324f5672
LeeSangHoon <dl57934@naver.com>
2018-04-29 23:41:58 +0900

    get Epl Team Img

--- get Sports Server/playingGameInfo.js
+++ get Sports Server/playingGameInfo.js
@@ -10,16 +10,14 @@ function getGameInfo(url,socket){
         }).then(function (status) {
             return _page.evaluate(function () {
                 var gameInfo= document.getElementById('_tab_box_epl').innerText;
-                var team1imgSrc = new Array();
-                var team2imgSrc = new Array();
-                var length = document.getElementsByTagName('li').length;
-                for (var i = 1; i<= length;i++){
+                var teamImg = new Array();
+                var teamLength = document.querySelectorAll('#_tab_box_epl > div > ul > li').length;
+                for (var i = 1; i <= teamLength;i++){
                     var teamImg1 = document.querySelector('#_tab_box_epl > div > ul > li:nth-child('+i+') > div.vs_list.vs_list1 > div > img').src;
-                    team1imgSrc.push(teamImg1);
+                    teamImg.push(teamImg1);
                     var teamImg2 = document.querySelector('#_tab_box_epl > div > ul > li:nth-child('+i+') > div.vs_list.vs_list2 > div > img').src;
-                    team2imgSrc.push(teamImg2);
+                    teamImg.push(teamImg2);
                 }
-                var teamImg = team1imgSrc.concat(team2imgSrc);
                 var game = {'gameInfo':gameInfo, 'teamImg':teamImg};
                 return game;
             });
