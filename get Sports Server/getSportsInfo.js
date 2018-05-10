67db345680d0b072d79f3ff9b601cf792c3db396
root <dl57934@naver.com>
2018-05-10 20:18:51 +0900

    edit date

--- get Sports Server/getSportsInfo.js
+++ get Sports Server/getSportsInfo.js
@@ -1,7 +1,7 @@
 var getHtml = require('./getHtml');
 var getGameInfo = require('./playingGameInfo');
 var date = new Date();
-var dd =date.getDate()-3;
+var dd =date.getDate();
 var mm = date.getMonth()+1;
 var yyyy = date.getFullYear();
 if(dd <10) dd='0'+dd;
